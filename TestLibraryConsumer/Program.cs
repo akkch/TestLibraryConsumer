7ee63af8976b4a8167aed56eaa22d7bd7d80c347
Evgeny Bulgakov <akkch1982@gmail.com>
2025-07-03 18:18:45 +0300

    Add project files.

---
+++ TestLibraryConsumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using TestLibrary;
+
+namespace TestLibraryConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole console = new MyConsole();
+            console.WriteLine("Hello, World! This is a test of the TestLibraryConsumer project.");
+        }
+    }
+}
